b974e5c97f8e679278bb80a16afeb7c04566575b
Toàn Từ <toantu03@gmail.com>
2025-04-20 23:13:29 +0700

    feat: add shop managements

---
+++ Form/Shop/Invoice.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Forms;
+using MongoDB.Driver;
+
+namespace MDBMS___E_COMMERCE_PLATFORM
+{
+    public partial class Invoice : Form
+    {
+        public Invoice(string email, MongoClient client)
+        {
+            InitializeComponent();
+        }
+    }
+}
